7f5a9222edda77455ac2d9924dfbb442bd5c9478
Ramiro Mangione <ramiromangionetello@gmail.com>
2024-07-04 11:17:39 -0300

    creacion de entorno

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Titulo from './componentes/Titulo'
+function App() {
+
+  return (
+      <div>
+        <h1>hola mundo</h1>
+        <Titulo/>
+        <Titulo/>
+      </div>
+  )
+}
+
+export default App
---
+++ src/componentes/Titulo.jsx
@@ -0,0 +1,10 @@
+const Titulo = () =>{
+
+    return (
+        <div>
+            <h2>Titulo</h2>
+        </div>
+    )
+}
+
+export default Titulo
